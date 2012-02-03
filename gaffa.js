580fbd05e387765e2f8a8129fd3c1fa6eede5ba6
kory <knunn187@gmail.com>
2012-02-03 13:02:17 +0000

    cleaned up get and set code, added comments, made some constants...

--- gaffa.js
+++ gaffa.js
@@ -1,7 +1,13 @@
 (function(undefined){
     
     //Create gaffa
-    window.gaffa = window.gaffa || newGaffa();
+    var gaffa = window.gaffa = window.gaffa || newGaffa();
+    
+    //"constants"
+    // functions to make it 'getter only'
+    gaffa.pathSeparator = function(){return "/";};
+    gaffa.upALevel = function(){return "..";};
+
     
     //internal varaibles
     var internalDataModel = {}, //these must always be objects.
@@ -11,46 +17,67 @@
     
     //Lost of similarities between get and set, refactor later to reuse code.
     function get(path, model){
-        var keys = path.split("/"),
+        var keys = path.split(gaffa.pathSeparator()),
             reference = model;
             
         for(var keyIndex = 0; keyIndex < keys.length; keyIndex++){
-            //Up a level.
-            if(keys[keyIndex] === ".."){
+            
+            //Up a level string. if encountered, knock out the previous key and the current one.
+            if(keys[keyIndex] === gaffa.upALevel()){
                 keys.splice(Math.max(keyIndex-1,0), keyIndex);
                 keyIndex--;
+                                
             }else{
+                
+                /*  
+                    if the thing at the current key in the model is an object
+                    or an array (both typeof to object),
+                    set it as the thing we want to look into next.
+                */
                 if(typeof reference[keys[keyIndex]] === "object"){
                     reference = reference[keys[keyIndex]];
-                }else if(reference[keys[keyIndex]] == undefined){
+                    
+                /* 
+                    else if there isn't anything at this key, exit the loop,
+                    and return undefined.
+                */   
+                }else if(reference[keys[keyIndex]] === undefined){
                     reference = undefined;
                     break;
+                    
+                /*  
+                    otherwise, we're at the end of the line. return whatever's
+                    there
+                */
                 }else{
                     reference = reference[keys[keyIndex]];
+                    break;
                 }
             }
         }
-        
+                
         return reference;   
     }
     
     function set(path, value, model){
         var currentValue = get(path, model),
             fireChanged = false,
-            keys = path.split("/"),
+            keys = path.split(gaffa.pathSeparator()),
             reference = model,
             keyIndex;
             
         for(keyIndex = 0; keyIndex < keys.length; keyIndex++){
             //Up a level.
-            if(keys[keyIndex] === ".."){
+            if(keys[keyIndex] === gaffa.upALevel()){
                 keys.splice(Math.max(keyIndex-1,0), keyIndex);
                 keyIndex--;
             }
         }
         
         for(keyIndex = 0; keyIndex < keys.length; keyIndex++){
-            //Up a level.
+            
+            //if we have hit a non-object and we have more keys after this one
+            //make an object (or array) here and move on.
             if(typeof reference[keys[keyIndex]] !== "object" && keyIndex < keys.length - 1){
                 if(!isNaN(keys[keyIndex + 1])){
                     reference[keys[keyIndex]] = [];
@@ -59,29 +86,33 @@
                     reference[keys[keyIndex]] = {};
                     reference = reference[keys[keyIndex]];
                 }
+                
+            // if we are at the end of the line, set to the model
             }else if(keyIndex === keys.length - 1){
                 reference[keys[keyIndex]] = value;
+                
+            //otherwise, RECURSANIZE!
             }else{
                 reference = reference[keys[keyIndex]];
             }
         }
         
         if(typeof currentValue === "object"){
-            fireChanged = true;
-            currentValue = value;
+            fireChanged = true; 
+            //to begin with we won't do a smart compare on objects, just fire changed every time.
+            //ToDo: smart compare, and fire events only for changed props.
         }else if(currentValue === value){
             fireChanged = true;
         }
         
         if(fireChanged){
-            $().trigger();
+            $(gaffa.model).trigger("change." + keys.join("."));
         }
     }
     
     function newGaffa(){
         
-        function gaffa(){
-            
+        function gaffa(){            
         }
         
         gaffa.prototype = {
