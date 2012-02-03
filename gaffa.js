445810d153e7fc9d242cd8cdda53342b23f67afc
kory <knunn187@gmail.com>
2012-02-03 11:17:43 +0000

    start of gaffa.js and a textbox view.

--- gaffa.js
+++ gaffa.js
@@ -1,6 +1,34 @@
 (function(undefined){
     
+    //Create gaffa
     window.gaffa = window.gaffa || newGaffa();
+    
+    //internal varaibles
+    var internalDataModel,
+        internalPresModel;
+    
+    //internal functions
+    function get(path, model){
+        var keys = path.split("/"),
+            reference = model;
+                            
+        for(var keyIndex = 0; keyIndex < keys.length; keyIndex++){
+            //Up a level.
+            if(keys[keyIndex] === ".."){
+                keys.splice(Math.max(keyIndex-1,0), keyIndex);
+                keyIndex--;
+            }else{
+                if(typeof reference[keys[keyIndex]] === "object"){
+                    reference = reference[keys[keyIndex]];
+                }else{
+                    reference = undefined;
+                    break;
+                }
+            }
+        }
+        
+        return reference;   
+    }
     
     function newGaffa(){
         
@@ -9,9 +37,37 @@
         }
         
         gaffa.prototype = {
-            
-            
-        }
+            presModel:{
+                get: function(path){
+                    return get(path, internalPresModel);
+                },
+                set: function(key, value){
+                    
+                },
+                update: function(key, value){
+                    
+                },
+            },
+            dataModel: {
+                get: function(path){
+                    return get(path, internalDataModel);
+                },
+                set: function(key, value){
+                    
+                },
+                update: function(key, value){
+                    
+                },
+            },
+            view: {
+                render: function(views){ //parameter optional.
+                    
+                },
+                add: function(views){
+                    
+                }
+            }
+        };
         
         return new gaffa();
     }
