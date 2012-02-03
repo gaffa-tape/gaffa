051b114941e5fb39e2b813abe3ce3f1b6138101a
kory <knunn187@gmail.com>
2012-02-03 09:44:38 +0000

    third go

--- gaffa.js
+++ gaffa.js
@@ -1,16 +1,18 @@
 (function(undefined){
     
-    window.gaffa = window.gaffa || gaffa();
+    window.gaffa = window.gaffa || newGaffa();
     
-    function gaffa(){
+    function newGaffa(){
         
+        function gaffa(){
+            
+        }
         
+        gaffa.prototype = {
+            
+            
+        }
         
-        
-        
-        
+        return new gaffa();
     }
-    
-    
-    
 })();
