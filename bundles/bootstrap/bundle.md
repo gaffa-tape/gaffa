57a11bc28f4e181e405dd21ca9ba319f7158004d
Matt <matt.ginty@gmail.com>
2012-02-03 21:56:29 +1000

    Adding Bootstrap Bundle

---
+++ bundles/bootstrap/bundle.md
@@ -0,0 +1,1 @@
+# Bundle: Twitter Bootstrap
---
+++ bundles/bootstrap/src/views/paragraph.js
@@ -0,0 +1,27 @@
+//	Properties:
+(function(undefined)) {
+	window.gaffa = window.gaffa || newGaffa();
+	window.gaffa.views = window.gaffa.views || {};
+	window.gaffa.views.paragraph = window.gaffa.views.paragraph || newView();
+
+	function createElement(viewModel) {
+		return $(document.createElement('p'));
+	}
+
+	function newView() {
+		
+		function view() {
+		}	
+		
+		view.prototype = {
+			render: function(viewModel) {
+				var element = viewModel.renderedElement = createElement(viewModel);
+				return element;
+			},
+			update: function(viewModel) {
+				var element = viewModel.renderedElement;
+			}
+		};
+		return new newView();
+	}
+}
