64c23a3b6ebfa32112f4d4d090d902b58e61298e
Matt <matt.ginty@gmail.com>
2012-02-09 08:40:18 +1000

    removing coffeescript from node example and adding makefile for gaffa

---
+++ Makefile
@@ -0,0 +1,31 @@
+VERSION=0.1
+DATE=$(shell DATE)
+GAFFA_FILE = gaffa.js
+GAFFA_SRC = ./${GAFFA_FILE}
+GAFFA_MIN = ./gaffa.min.js
+ACTIONS_FOLDER = ./actions/
+VIEWS_FOLDER = ./views/
+DROP_FOLDERS = ./examples/node/public/scripts/
+LESS_COMPRESSOR ?= `which lessc`
+UGLIFY_JS ?= `which uglifyjs`
+
+build:
+	@@if test ! -z ${UGLIFY_JS}; then \
+		echo "build @ "${DATE}; \
+		echo "minifying source";\
+		uglifyjs -o ${GAFFA_MIN} ${GAFFA_SRC};\
+		for dropFolder in ${DROP_FOLDERS}; do \
+			echo "copying to drop folder:" $$dropFolder;\
+			cp $(GAFFA_SRC) $$dropFolder;\
+			for viewFile in ${VIEWS_FOLDER}*.js; do \
+				cat $$viewFile >> $$dropFolder/views.js; \
+			done; \
+			for actionFile in ${ACTIONS_FOLDER}*.js; do \
+				cat $$actionFile >> $$dropFolder/actions.js; \
+			done; \
+		done; \
+		echo "complete";\
+	else \
+		echo "You must have the UGLIFYJS minifier installed in order to minify gaffa.js."; \
+		echo "You can install it by running: (sudo) npm install uglify-js -g"; \
+	fi
---
+++ examples/node/public/scripts/actions.js
@@ -0,0 +1,41 @@
+//    Properties:
+//        styles: container | container-fluid | row | row-fluid | span* | offset*
+(function(undefined) {
+    var actionType = "fromJson";
+    window.gaffa.actions[actionType] = function(action){
+        if(gaffa.utils.propExists(action, "bindings.setFrom.binding")){
+            gaffa.model.set(action.bindings.setTo.binding, JSON.parse(gaffa.model.get(action.bindings.setFrom.binding)));            
+        }else{
+            gaffa.model.set(action.bindings.setTo.binding, JSON.parse(action.bindings.setFrom.value));            
+        }
+    };
+})();//    Properties:
+//        styles: container | container-fluid | row | row-fluid | span* | offset*
+(function(undefined) {
+    var actionType = "set";
+    window.gaffa.actions[actionType] = function(action){
+        if(gaffa.utils.propExists(action, "bindings.setFrom.binding")){
+            gaffa.model.set(action.bindings.setTo.binding, gaffa.model.get(action.bindings.setFrom.binding));            
+        }else{
+            gaffa.model.set(action.bindings.setTo.binding, action.bindings.setFrom.value);            
+        }
+    };
+})();//    Properties:
+//        styles: container | container-fluid | row | row-fluid | span* | offset*
+(function(undefined) {
+    var actionType = "toJson";
+    window.gaffa.actions[actionType] = function(action){
+        if(gaffa.utils.propExists(action, "bindings.setFrom.binding")){
+            gaffa.model.set(action.bindings.setTo.binding, JSON.stringify(gaffa.model.get(action.bindings.setFrom.binding)));            
+        }else{
+            gaffa.model.set(action.bindings.setTo.binding, JSON.stringify(action.bindings.setFrom.value));            
+        }
+    };
+})();//    Properties:
+//        styles: container | container-fluid | row | row-fluid | span* | offset*
+(function(undefined) {
+    var actionType = "toggle";
+    window.gaffa.actions[actionType] = function(action){
+        gaffa.model.set(action.bindings.toggle.binding, !gaffa.model.get(action.bindings.toggle.binding));
+    };
+})();
