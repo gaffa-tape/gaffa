1959638a03cb8cea67dbb6fc8c0f03674ddef8fd
ide user ide_kory <knunn187@gmail.com>
2012-06-09 03:13:47 +0000

    deleting extra files

--- actions/toggle.js
+++
@@ -1,8 +0,0 @@
-//    Properties:
-//        styles: container | container-fluid | row | row-fluid | span* | offset*
-(function(undefined) {
-    var actionType = "toggle";
-    window.gaffa.actions[actionType] = function(action){
-        gaffa.model.set(action.bindings.toggle.binding, !gaffa.model.get(action.bindings.toggle.binding));
-    };
-})();
