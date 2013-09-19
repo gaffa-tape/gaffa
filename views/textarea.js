d5b0558fc9f8faa9d4bc7a7518c327a838366983
Kory Nunn <knunn187@gmail.com>
2013-09-19 11:00:46 +1000

    fixed textaread

--- views/textarea.js
+++ views/textarea.js
@@ -3,35 +3,35 @@ var Gaffa = require('gaffa'),
     doc = require('doc-js'),
     viewType = "textarea",
 	cachedElement;
-    
+
 function Textarea(){}
 Textarea = Gaffa.createSpec(Textarea, Gaffa.View);
 Textarea.prototype.type = viewType;
 
-Textarea.prototype.render = function(){        
+Textarea.prototype.render = function(){
     var view = this,
         renderedElement = crel('textarea');
-    
+
     doc.on(this.updateEventName || "change", renderedElement, function(){
         view.value.set(renderedElement.value);
     });
-    
+
     this.renderedElement = renderedElement;
-    
+
     this.__super__.render.apply(this, arguments);
 };
 
-Textarea.prototype.value = new Gaffa.Property(Gaffa.propertyUpdaters.string(function(viewModel, value){
-    viewModel.renderedElement.value = value;
-}));
+Textarea.prototype.value = new Gaffa.Property(function(view, value){
+    view.renderedElement.value = value;
+});
 
-Textarea.prototype.placeholder = new Gaffa.Property(Gaffa.propertyUpdaters.string(function(viewModel, value){
-    viewModel.renderedElement[value ? 'setAttribute' : 'removeAttribute']('placeholder', value);
+Textarea.prototype.placeholder = new Gaffa.Property(function(view, value){
+    view.renderedElement[value ? 'setAttribute' : 'removeAttribute']('placeholder', value);
 
-}));
+});
 
-Textarea.prototype.disabled = new Gaffa.Property(Gaffa.propertyUpdaters.bool(function(viewModel, value){
-    viewModel.renderedElement[value ? 'setAttribute' : 'removeAttribute']('disabled', value);
-}));
+Textarea.prototype.disabled = new Gaffa.Property(function(view, value){
+    view.renderedElement[value ? 'setAttribute' : 'removeAttribute']('disabled', value);
+});
 
 module.exports = Textarea;
