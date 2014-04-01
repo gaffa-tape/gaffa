2ce275821c1ce3f9a1b8a368b5f478dc0cbe50f1
KoryNunn <knunn187@gmail.com>
2014-04-01 13:43:52 +1000

    Fixed _type serialisation issue

--- gaffa.js
+++ gaffa.js
@@ -531,7 +531,7 @@ function jsonConverter(object, exclude, include){
     var plainInstance = new object.constructor(),
         tempObject = Array.isArray(object) || object instanceof Array && [] || {},
         excludeProps = ["gaffa", "parent", "parentContainer", "renderedElement", "_removeHandlers", "gediCallbacks", "__super__", "_events"],
-        includeProps = ["type"];
+        includeProps = ["type", "_type"];
 
     //console.log(object.constructor.name);
 
