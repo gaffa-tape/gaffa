60fe3b5f3e4bbb4f407e01d5acef77a82d03f15b
KoryNunn <knunn187@gmail.com>
2014-06-19 13:45:23 +1000

    Fixed view adding bug

--- gaffa.js
+++ gaffa.js
@@ -684,6 +684,8 @@ ViewContainer.prototype.getPath = function(){
 ViewContainer.prototype.add = function(view, insertIndex){
     // If passed an array
     if(Array.isArray(view)){
+        // Clone the array so splicing can't cause issues
+        var views = view.slice();
         for(var i = 0; i < view.length; i++){
             this.add(view[i]);
         }
@@ -691,7 +693,7 @@ ViewContainer.prototype.add = function(view, insertIndex){
     }
 
     // Is already in the tree somewhere? remove it.
-    if(view.parentContainer){
+    if(view.parentContainer instanceof ViewContainer){
         view.parentContainer.splice(view.parentContainer.indexOf(view),1);
     }
 
